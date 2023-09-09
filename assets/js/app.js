247f91fd7685e9f22efb3c55fc1703f31ec181e9
Naman Kumar <namankr02@gmail.com>
2023-09-09 13:49:59 +0530

    Initial GitHub pages site with Jekyll

--- assets/js/app.js
+++
@@ -1,6 +0,0 @@
-(function($){
-	$("#preprint-publications, #conference-publications, #journal-publications, #thesis-publications").on("click", "a.show-abstract", function(e){
-		e.preventDefault();
-		$(this).closest("li").find("div.abstract-details").toggleClass("hide");
-	});
-})(jQuery);
